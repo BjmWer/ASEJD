bbb3a75b2a33e70776a49e8af2bf410848f46238
BjmWer <148626493+BjmWer@users.noreply.github.com>
2024-09-24 23:42:23 +0800

    Update search.js
    写了一个注释，用以标注已发现，但却弄不懂的一个bug

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ let search_txt = document.querySelector('.search_txt')
 const search_btn = document.querySelector('.search_btn')
 let shiyi = document.querySelector('.shiyi')
 //检验是否为农业
-search_btn.addEventListener('click', function () {
+search_btn.addEventListener('click', function () { //这里为什么说addEventListener无法获取元素啊？？
   if (search_txt.value.includes('农业')) {
     shiyi.innerHTML = '欧尼'
   } else {
